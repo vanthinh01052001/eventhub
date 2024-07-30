faf77cf3e79d9f16c32c3f4dbb952772ee798b82
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-30 17:03:14 +0700

    add onboarding screen

--- src/navigators/AuthNavigator.tsx
+++ src/navigators/AuthNavigator.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { LoginScreen } from '../screens';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {LoginScreen} from '../screens';
+import OnboadingScreen from '../screens/auth/OnboadingScreen';
 
 const AuthNavigator = () => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
   return (
-    <Stack.Navigator 
-        screenOptions={{
-            headerShown: false
-        }}
-    >
-        <Stack.Screen name='LoginScreen' component={LoginScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="OnboardingScreen" component={OnboadingScreen} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
     </Stack.Navigator>
-  )
-}
+  );
+};
 
-export default AuthNavigator
+export default AuthNavigator;
