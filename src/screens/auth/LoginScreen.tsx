7c6ac714ebe444841e1d6d6ec84117fac2b4f468
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-07-30 15:31:00 +0700

    navigate from splash screen to main and login page

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   return (
     <View>
       <Text>LoginScreen</Text>
+      <Button
+        title="Login"
+        onPress={async () =>
+          await AsyncStorage.setItem('assetToken', 'ahgdugfi')
+        }
+      />
     </View>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,12 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, Button} from 'react-native';
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
   return (
-    <View>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text>HomeScreen</Text>
+      <Button title="Logout" onPress={async () => await AsyncStorage.clear()} />
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
