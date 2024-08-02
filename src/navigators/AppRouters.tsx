2af27a899879fe3f17bd369c1874eb26261f06cd
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-08-02 17:04:05 +0700

    updated UI

--- src/navigators/AppRouters.tsx
+++ src/navigators/AppRouters.tsx
@@ -1,24 +1,42 @@
 import {View, Text} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNavigator from './MainNavigator';
 import AuthNavigator from './AuthNavigator';
 import {useAsyncStorage} from '@react-native-async-storage/async-storage';
 import {useDispatch, useSelector} from 'react-redux';
 import {addAuth, authSelector} from '../redux/reducers/authReducer';
+import {SplashScreen} from '../screens';
 
 const AppRouters = () => {
   const {getItem} = useAsyncStorage('auth');
   const auth = useSelector(authSelector);
   const dispatch = useDispatch();
+  const [isShowSplash, setIsShowSplash] = useState(true);
+
   useEffect(() => {
     checkLogin();
+    const timeout = setTimeout(() => {
+      setIsShowSplash(false);
+    }, 1500);
+    return () => clearTimeout(timeout);
   }, []);
+
   const checkLogin = async () => {
     const res = await getItem();
     res && dispatch(addAuth(JSON.parse(res)));
   };
 
-  return <>{auth.accessToken ? <MainNavigator /> : <AuthNavigator />}</>;
+  return (
+    <>
+      {isShowSplash ? (
+        <SplashScreen />
+      ) : auth.accessToken ? (
+        <MainNavigator />
+      ) : (
+        <AuthNavigator />
+      )}
+    </>
+  );
 };
 
 export default AppRouters;
