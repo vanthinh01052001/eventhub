126c0f93f410fc2ecfdc6cb3f4824cc64a443c50
vanthinh01052001 <vanthinh01052001@gmail.com>
2024-08-07 16:41:08 +0700

    add navigator bottom tab

--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,16 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { HomeScreen } from '../screens';
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {HomeScreen} from '../screens';
+import ExploreNavigator from './ExploreNavigator';
 
 const TabNavigator = () => {
-    const Tab = createBottomTabNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name='HomeScreen' component={HomeScreen} />
+      <Tab.Screen name="Explore" component={ExploreNavigator} />
     </Tab.Navigator>
-  )
-}
+  );
+};
 
-export default TabNavigator
+export default TabNavigator;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,8 +2,17 @@ import ForgotPassword from "./auth/ForgotPassword";
 import LoginScreen from "./auth/LoginScreen";
 import SignUpScreen from "./auth/SignUpScreen";
 import Verification from "./auth/Verification";
+
 import HomeScreen from "./home/HomeScreen";
+
+import EventsScreen from "./events/EventsScreen";
+
+import MapScreen from "./maps/MapScreen";
+
+import ProfileScreen from "./profiles/ProfileScreen";
+
 import SplashScreen from "./SplashScreen";
+import AddNewScreen from "./AddNewScreen";
 
 export {
     HomeScreen,
@@ -11,5 +20,9 @@ export {
     SplashScreen,
     ForgotPassword,
     SignUpScreen,
-    Verification
+    Verification,
+    AddNewScreen,
+    EventsScreen,
+    MapScreen,
+    ProfileScreen
 }
